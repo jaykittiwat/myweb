72cfee58532e3eceeec64cc535ea42990501088d
jaykittiwat <jaykittiwat2542@gmail.com>
2020-04-06 19:22:42 +0700

    update to remote githup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-
+import {Button} from "react-bootstrap";
 class App extends Component{
   render(){
     return(
       <div>
-              Hello world
+        <Button type="button" name="" id="" class="btn btn-primary" btn-lg   btn-block> 123 </Button>
       </div>
     )
   }
