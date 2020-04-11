16156defc1d0c6c42cd84d958eb38c6ae8489857
jaykittiwat <jaykittiwat2542@gmail.com>
2020-04-11 21:07:23 +0700

    update to romote  githup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import Monitor from './Monitor';
+import {BrowserRouter,Route,Switch} from "react-router-dom";
+import Login from './component/Login/login';
 
+
+// Router to path component
 class App extends Component{
   
+  renderRouter(){
+    return(
+      <Switch>
+        <Route exact path="/" component={Monitor}/>
+        <Route path="/login" component={Login}/>
+      </Switch>
+    )
+  }
   render(){
     return(
       <div>
 
-        <Monitor/>
+      <BrowserRouter>{this.renderRouter()}</BrowserRouter>
   
       </div>
     )
--- src/component/Login/login.js
+++ src/component/Login/login.js
@@ -16,32 +16,41 @@ class Login extends Component{
                     <div className="col-md-1 r2-c2">ลงทะเบียน</div>
                     <div className="col-md-1 r2-c2 ">เข้าสู่ระบบ</div>
                 </div>
+
+
                 <div className="row">
               <div className="image-bg"> 
-              
-              <div className="bg-box-log-color">
-              <Form  className="form-padding">
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>ไอดีผู้ใช้</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" /> 
-              </Form.Group>
-            
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>รหัสผ่าน</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="จำรหัสผ่าน" />
-              </Form.Group>
-              <Button variant="primary">
-               เข้าสู่ระบบ
-              </Button>
-            </Form>
-            </div>
-              
+                    <div className="bg-box-log-color">
+                    <Form  className="form-padding ">
+                    <div className="title1">เข้าสู่ระบบ</div>
+                    <Form.Group controlId="formBasicEmail" className="form-padding-top">
+                      <Form.Label>ไอดีผู้ใช้</Form.Label>
+                      <Form.Control type="email" placeholder="Enter email" /> 
+                    </Form.Group>
+                  
+                    <Form.Group controlId="formBasicPassword">
+                      <Form.Label>รหัสผ่าน</Form.Label>
+                      <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                      <Form.Check type="checkbox" label="จำรหัสผ่าน" />
+                    </Form.Group>
+                    <Button className="container-fluid bt " href="/" aria-disabled="true" >
+                    เข้าสู่ระบบ
+                    </Button>
+                  </Form>
+                  </div>
+                    
               </div>
                 </div>
-                     
+              
+                <div className="footer-style2 row ">
+                  <div className="row footer-padding container-fluid">
+                  <div className="col-md-4"><h3>ผู้พัฒนา</h3><ul><li>นายกิตติวัฒน์ ศรีชัยพล</li><li>นายไชยวัฒน์  อธิกรม</li></ul></div> 
+                  <div className="col-md-4"><h3>ที่ปรึกษา</h3><ul><li>ดร.ภูวิศสรณ์ ภูมิสรณคมณ์</li></ul></div> 
+                  <div className="col-md-4"><h3>ติดต่อได้ที่</h3><ul><li>0928412006</li></ul></div>
+                  </div>
+                </div>
                      
                          
                      
@@ -53,4 +62,3 @@ class Login extends Component{
 }
 export default Login;
 
-/***/
