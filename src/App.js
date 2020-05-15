b216ba53c69a4baf055598fadfa9c3e9a883134a
theframeza77 <63237834+theframeza77@users.noreply.github.com>
2020-05-15 17:51:28 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import {BrowserRouter,Route,Switch} from "react-router-dom";
 //import Loginpage from './component/Login/Loginpage';
 import Registor from './component/Login/registor';
 import login from "./component/Login/login";
+<<<<<<< HEAD
 import cow from "././component/cowpage/cow"
+=======
+>>>>>>> parent of 1ca2199... Update App.js
 // Router to path component
 class App extends Component{
   
@@ -14,7 +17,10 @@ class App extends Component{
         <Route exact path="/" component={Monitor}/>
         <Route path="/login" component={login}/>
         <Route path="/registor" component={Registor}/>
+<<<<<<< HEAD
         <Route path="/letsgo" component={cow}/>
+=======
+>>>>>>> parent of 1ca2199... Update App.js
       </Switch>
     )
   }
