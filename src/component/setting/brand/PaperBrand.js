5b34b9204fd5bb82bf539d6fe5e17a2a34ede66e
theframeza77 <63237834+theframeza77@users.noreply.github.com>
2020-06-20 02:19:03 +0700

    update to remote githup

--- src/component/setting/brand/PaperBrand.js
+++ src/component/setting/brand/PaperBrand.js
@@ -3,7 +3,6 @@ import "./../styleSetting.css";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
-
 import Button from "@material-ui/core/Button";
 import SaveIcon from "@material-ui/icons/Save";
 
