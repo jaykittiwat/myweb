a010d041dc042ab04ec4b85a04d92863017013d7
theframeza77 <63237834+theframeza77@users.noreply.github.com>
2020-07-01 20:47:10 +0700

    update to remote githup

--- src/HeaderLogin.js
+++ src/HeaderLogin.js
@@ -11,7 +11,7 @@ class HeaderLogin extends Component{
           <div className='container-fluid'>
               <div className='row '>
                  <div className="col-md-5"> 
-                <div className="row container-fluid r2-c1 ">เว็บแอปพลิเคชันระบบการจัดการแม่พันธุ์โค</div> 
+                <div className="row container-fluid r2-c1">เว็บแอปพลิเคชันระบบการจัดการแม่พันธุ์โค</div> 
                 <div className="row  container-fluid r2-c1-t2">Webapplication Dam Management System</div>
             </div> 
             </div>    
